2738df3ce7519df00d1b34a3be73f4e5059223c5
redwac <sabbane.reda@gmail.com>
2021-06-21 10:02:41 +0000

    Initial commit

---
+++ src/test/java/com/example/springBootOne/SpringBootOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
